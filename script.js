6b69534d8bd4fd2a4ea2c5e6dd0ce677372d85c5
musfira0214 <musfirarazzaq17331@gmail.com>
2023-10-25 12:30:18 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle')
+
+    const banner = document.querySelector('.banner')
+
+    menuToggle.classList.toggle('active')
+
+    banner.classList.toggle('active')
+}
